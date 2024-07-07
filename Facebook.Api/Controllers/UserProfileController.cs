91a4e7b9981cdd6a5cd165e9e75fcc31728803d4
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-07 14:15:40 +0300

    feat: return updated profile ✨

--- Facebook.Api/Controllers/UserProfileController.cs
+++ Facebook.Api/Controllers/UserProfileController.cs
@@ -14,52 +14,52 @@ namespace Facebook.Server.Controllers;
 [ApiController]
 public class UserProfileController(ISender mediatr, IMapper mapper) : ApiController
 {
-   [HttpPut("edit-profile")]
-   public async Task<IActionResult> EditProfileAsync([FromForm] UserEditProfileRequest request)
-   {
-      var coverPhoto = new byte[request.CoverPhoto == null ? 0 : request.CoverPhoto.Length];
-      var avatar = new byte[request.Avatar == null ? 0 : request.Avatar.Length];
+    [HttpPut("edit-profile")]
+    public async Task<IActionResult> EditProfileAsync([FromForm] UserEditProfileRequest request)
+    {
+        var coverPhoto = new byte[request.CoverPhoto == null ? 0 : request.CoverPhoto.Length];
+        var avatar = new byte[request.Avatar == null ? 0 : request.Avatar.Length];
 
-      if (request.CoverPhoto != null && request.CoverPhoto.Length != 0)
-      {
-         using MemoryStream memoryStream = new MemoryStream();
-         await request.CoverPhoto.CopyToAsync(memoryStream);
+        if (request.CoverPhoto != null && request.CoverPhoto.Length != 0)
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            await request.CoverPhoto.CopyToAsync(memoryStream);
 
-         coverPhoto = memoryStream.ToArray();
-      }
-      if (request.Avatar != null && request.Avatar.Length != 0)
-      {
-         using MemoryStream memoryStream = new MemoryStream();
-         await request.Avatar.CopyToAsync(memoryStream);
+            coverPhoto = memoryStream.ToArray();
+        }
+        if (request.Avatar != null && request.Avatar.Length != 0)
+        {
+            using MemoryStream memoryStream = new MemoryStream();
+            await request.Avatar.CopyToAsync(memoryStream);
 
-         avatar = memoryStream.ToArray();
-      }
+            avatar = memoryStream.ToArray();
+        }
 
-      var editResult = await mediatr.Send(
-          mapper.Map<UserEditProfileCommand>((request, coverPhoto, avatar)));
+        var editResult = await mediatr.Send(
+            mapper.Map<UserEditProfileCommand>((request, coverPhoto, avatar)));
 
-      return editResult.Match(
-          authResult => Ok(),
-          errors => Problem(errors));
-   }
-   [HttpDelete("delete-profile")]
-   public async Task<IActionResult> DeleteProfileAsync([FromForm] DeleteRequest request)
-   {
-      var command = mapper.Map<DeleteUserCommand>(request);
-      var deleteResult = await mediatr.Send(command);
+        return editResult.Match(
+            authResult => Ok(editResult.Value),
+            errors => Problem(errors));
+    }
+    [HttpDelete("delete-profile")]
+    public async Task<IActionResult> DeleteProfileAsync([FromForm] DeleteRequest request)
+    {
+        var command = mapper.Map<DeleteUserCommand>(request);
+        var deleteResult = await mediatr.Send(command);
 
-      return deleteResult.Match(
-      deleteRes => Ok(),
-      errors => Problem(errors));
-   }
-   [HttpGet("get-profile-by-id")]
-   public async Task<IActionResult> GetUserProfileByIdAsync([FromQuery] GetUserProfileByIdRequest request)
-   {
-      var query = mapper.Map<GetUserProfileByIdQuery>(request);
-      var getRes = await mediatr.Send(query);
+        return deleteResult.Match(
+        deleteRes => Ok(),
+        errors => Problem(errors));
+    }
+    [HttpGet("get-profile-by-id")]
+    public async Task<IActionResult> GetUserProfileByIdAsync([FromQuery] GetUserProfileByIdRequest request)
+    {
+        var query = mapper.Map<GetUserProfileByIdQuery>(request);
+        var getRes = await mediatr.Send(query);
 
-      return getRes.Match(
-      getRes => Ok(getRes),
-      errors => Problem(errors));
-   }
+        return getRes.Match(
+        getRes => Ok(getRes),
+        errors => Problem(errors));
+    }
 }

fa37794d560466e0eb98cf3667ddf98580fa4093
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-07-16 09:04:13 +0300

    feat: added AcceptFriendRequest file for better experience

---
+++ Facebook.Contracts/Friends/AcceptFriendRequest.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Facebook.Contracts.Friends;
+
+public record AcceptFriendRequest
+{
+    [Required(ErrorMessage = "{PropertyName} must not be empty")]
+    public Guid UserId { get; set; }
+    
+    [Required(ErrorMessage = "{PropertyName} must not be empty")]
+    public Guid FriendRequestId { get; set; }
+}

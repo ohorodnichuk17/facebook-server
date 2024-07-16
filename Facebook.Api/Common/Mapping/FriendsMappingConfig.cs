da7e003ab699e45a0fe4d4c64e8917febe107e28
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-16 11:06:00 +0300

    feat: send email when send-friend-request ✨

--- Facebook.Api/Common/Mapping/FriendsMappingConfig.cs
+++ Facebook.Api/Common/Mapping/FriendsMappingConfig.cs
@@ -9,22 +9,23 @@ namespace Facebook.Server.Common.Mapping;
 
 public class FriendsMappingConfig : IRegister
 {
-   public void Register(TypeAdapterConfig config)
-   {
-      config.NewConfig<FriendRequest, AcceptFriendRequestCommand>()
-          .Map(dest => dest.UserId, src => src.UserId)
-          .Map(dest => dest.FriendRequestId, src => src.FriendId);
+    public void Register(TypeAdapterConfig config)
+    {
+        config.NewConfig<FriendRequest, AcceptFriendRequestCommand>()
+            .Map(dest => dest.UserId, src => src.UserId)
+            .Map(dest => dest.FriendId, src => src.FriendId);
 
-      config.NewConfig<FriendRequest, SendFriendRequestCommand>()
-          .Map(dest => dest.UserId, src => src.UserId)
-          .Map(dest => dest.FriendId, src => src.FriendId);
+        config.NewConfig<(FriendRequest request, string BaseUrl), SendFriendRequestCommand>()
+            .Map(dest => dest.UserId, src => src.request.UserId)
+            .Map(dest => dest.FriendId, src => src.request.FriendId)
+            .Map(dest => dest.baseUrl, src => src.BaseUrl);
 
-      config.NewConfig<FriendRequest, RejectFriendRequestCommand>()
-          .Map(dest => dest.UserId, src => src.UserId)
-          .Map(dest => dest.FriendRequestId, src => src.FriendId);
+        config.NewConfig<FriendRequest, RejectFriendRequestCommand>()
+            .Map(dest => dest.UserId, src => src.UserId)
+            .Map(dest => dest.FriendRequestId, src => src.FriendId);
 
-      config.NewConfig<FriendRequest, RemoveFriendCommand>()
-          .Map(dest => dest.UserId, src => src.UserId)
-          .Map(dest => dest.FriendId, src => src.FriendId);
-   }
+        config.NewConfig<FriendRequest, RemoveFriendCommand>()
+            .Map(dest => dest.UserId, src => src.UserId)
+            .Map(dest => dest.FriendId, src => src.FriendId);
+    }
 }
--- Facebook.Application/User/Friends/Command/SendFriendRequest/SendFriendRequestCommand.cs
+++ Facebook.Application/User/Friends/Command/SendFriendRequest/SendFriendRequestCommand.cs
@@ -5,5 +5,6 @@ namespace Facebook.Application.User.Friends.Command.SendFriendRequest;
 
 public record SendFriendRequestCommand(
     Guid UserId,
-    Guid FriendId
+    Guid FriendId,
+    string baseUrl
 ) : IRequest<ErrorOr<Unit>>;

32ff828bf554481c5f0961a772cdb390cad8cc85
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-19 16:19:16 +0300

    feat: delete user endpoint for admin ✨

---
+++ Facebook.Api/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Facebook.Application.UserProfile.Command.DeleteUser;
+using Facebook.Contracts.DeleteRequest;
+using Facebook.Domain.Constants.Roles;
+using MapsterMapper;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Facebook.Server.Controllers;
+
+[Route("api/admin")]
+[ApiController]
+[Authorize(Roles = Roles.Admin)]
+public class AdminController(ISender mediatr, IMapper mapper, IConfiguration configuration)
+    : ApiController
+{
+    [HttpDelete("delete")]
+    public async Task<IActionResult> DeleteProfileAsync([FromForm] DeleteRequest request)
+    {
+        var command = mapper.Map<DeleteUserCommand>(request);
+        var deleteResult = await mediatr.Send(command);
+
+        return deleteResult.Match(
+            deleteRes => Ok(),
+            errors => Problem(errors));
+    }
+}

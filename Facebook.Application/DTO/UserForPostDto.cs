91db4deba37116173df485db26d73268196e7233
kutukov2000 <kutukov16.2005@gmail.com>
2024-07-25 13:09:18 +0300

    feat: add Id field in UserForPostDto ✨

--- Facebook.Application/DTO/UserForPostDto.cs
+++ Facebook.Application/DTO/UserForPostDto.cs
@@ -2,6 +2,7 @@
 
 public class UserForPostDto
 {
+    public Guid Id { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public DateTime Birthday { get; set; }

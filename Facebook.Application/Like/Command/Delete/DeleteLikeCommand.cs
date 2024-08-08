198319b1f4025fce14f0fbd3112c87eec0181cb9
kutukov2000 <kutukov16.2005@gmail.com>
2024-08-08 17:06:11 +0300

    refactor: remove unused usings 📦

--- Facebook.Application/Like/Command/Delete/DeleteLikeCommand.cs
+++ Facebook.Application/Like/Command/Delete/DeleteLikeCommand.cs
@@ -1,13 +1,8 @@
 ﻿using ErrorOr;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Facebook.Application.Like.Command.Delete;
 
 public record DeleteLikeCommand(
-    Guid Id    
+    Guid Id
 ) : IRequest<ErrorOr<bool>>;

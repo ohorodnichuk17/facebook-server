e49c0d02e75cad03630f308ef91b3210290e2b0a
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-17 17:57:59 +0300

    fix error with friend recomendations

--- Facebook.Application/User/Friends/Query/GetFriendsRecommendations/GetFriendsRecommendationsQueryHandler.cs
+++ Facebook.Application/User/Friends/Query/GetFriendsRecommendations/GetFriendsRecommendationsQueryHandler.cs
@@ -3,13 +3,9 @@ using Facebook.Application.Common.Interfaces.Common;
 using Facebook.Application.Common.Interfaces.IUnitOfWork;
 using Facebook.Domain.User;
 using MediatR;
-using Microsoft.AspNetCore.Identity;
-
 namespace Facebook.Application.User.Friends.Query.GetFriendsRecommendations;
-
 public class GetFriendsRecommendationsQueryHandler(
     IUnitOfWork unitOfWork,
-    UserManager<UserEntity> userManager,
     ICurrentUserService currentUserService)
     : IRequestHandler<GetFriendsRecommendationsQuery, ErrorOr<IEnumerable<UserEntity>>>
 {
@@ -17,37 +13,18 @@ public class GetFriendsRecommendationsQueryHandler(
     {
         try
         {
-            string currentUserId = currentUserService.GetCurrentUserId();
-
             var allUsers = await unitOfWork.User.GetAllUsersAsync();
-            var friends = await unitOfWork.User.GetAllFriendsAsync(currentUserId);
 
             if (allUsers.IsError || allUsers.Value.Count == 0)
             {
                 return Error.NotFound("No users found for recommendations");
             }
 
-            var friendIds = friends.Select(f => f.Id.ToString()).ToHashSet();
-
-            var filteredUsers = allUsers.Value
-                .Where(u => u.Id.ToString() != currentUserId)
-                .Where(u => !friendIds.Contains(u.Id.ToString()))
-                .ToList();
-
-            var nonAdminUsers = new List<UserEntity>();
-
-            foreach (var user in filteredUsers)
-            {
-                var roles = await userManager.GetRolesAsync(user);
-                if (!roles.Contains("admin"))
-                {
-                    nonAdminUsers.Add(user);
-                }
-            }
+            string currentUserId = currentUserService.GetCurrentUserId();
 
             var random = new Random();
-            var recommendations = nonAdminUsers
-                .OrderBy(u => random.Next())
+            var recommendations = allUsers.Value.Where(u => u.Id.ToString() != currentUserId)
+                .OrderBy(u => random.Next()) 
                 .Take(20)
                 .ToList();
 

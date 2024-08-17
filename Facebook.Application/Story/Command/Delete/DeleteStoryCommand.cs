986f765b62a04356f0d32718be3fc2478d79d599
Julia Ohorodnichuk <ogorodnicukulia@gmail.com>
2024-08-17 15:40:27 +0300

    fix: story deletion

--- Facebook.Application/Story/Command/Delete/DeleteStoryCommand.cs
+++ Facebook.Application/Story/Command/Delete/DeleteStoryCommand.cs
@@ -5,6 +5,10 @@ namespace Facebook.Application.Story.Command.Delete;
 
 public record DeleteStoryCommand(Guid Id) : IRequest<ErrorOr<bool>>
 {
+    public DeleteStoryCommand() : this(Guid.Empty)
+    {
+    }
+    
     public DeleteStoryCommand(string requestId) : this(Guid.Parse(requestId))
     {
     }
